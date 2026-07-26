9f5e0707ac2dd232034567ed91fbe87507d32caf
divyav-tech <divyaverma4889@gmail.com>
2026-07-26 21:35:54 +0530

    Learnt Basics of JavaScript

---
+++ Web Designing/JavaScript/Day 20-Basics Of JavaScript/myscript.js
@@ -0,0 +1,27 @@
+var x = 25;
+var y = 25;
+var c = x + y;
+var d = "<br> <b> hello </b> <br> ";
+document.write(c);
+document.write(d);
+document.write(d);
+document.write(x * c);
+document.write(x * y);
+document.write(x + 10);
+document.write(x / 10);
+
+
+/*document.write(c);
+ document.write(c);
+  document.write(c);*/
+document.write(c);
+document.write(c);
+
+//document.write(c);
+
+
+
+
+
+
+
